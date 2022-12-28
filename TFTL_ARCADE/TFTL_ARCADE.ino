fa1e51572fd723e992e12f2ace4771ca5ea0a10a
Qwerty-techlab <85824350+Qwerty-techlab@users.noreply.github.com>
2022-12-28 23:41:17 +0700

    Add files via upload

--- TFTL_ARCADE/TFTL_ARCADE.ino
+++ TFTL_ARCADE/TFTL_ARCADE.ino
@@ -1,11 +1,26 @@
+/*
+  TFTL-ARCADE
+
+  this sketch works only on Arduino Leonardo 
+  and similar bords (like as pro micro).
+
+  created 24 Dec 2022
+  modified 28 Dec 2022
+  by Kirill Chefonov
+
+  GitHub: https://github.com/Qwerty-techlab
+  VK: https://vk.com/alkekseevich 
+*/
+
 //--------------settings------------------
-#define BTN_1 1
-#define BTN_2 2
-#define BTN_3 3
-#define GAS_1 4
+#define BTN_1 1 //left btn
+#define BTN_2 2 //right btn
+#define BTN_3 3 //start btn
+#define GAS_1 4 
 #define GAS_2 5
 
-#define DT1 6
+//encoder(wheel) pins. 1-left encoder, 2-right encoder
+#define DT1 6  
 #define DT2 8
 #define SW1 7
 #define SW2 9
@@ -38,15 +53,21 @@ void loop(){
 
   first.tick();
   second.tick();
-//----------------------------------------
-  if(first.isRight()) Keyboard.print('q');
-  if(first.isLeft()) Keyboard.print('w');
-  if(second.isRight()) Keyboard.print('e');
-  if(second.isLeft()) Keyboard.print('r');
 
-  if(gas1.isHold()) Keyboard.print('t');
-  if(gas2.isHold()) Keyboard.print('y');
+//=======================================================
 
-
-
+//-----------------------wheel---------------------------
+  if(first.isRight()) Keyboard.print(KEY_RIGHT_ARROW);
+  if(first.isLeft()) Keyboard.print(KEY_LEFT_ARROW);
+  if(second.isRight()) Keyboard.print('a');
+  if(second.isLeft()) Keyboard.print('d');
+//----------------------pedals---------------------------
+  if(gas1.isHold()) Keyboard.print(KEY_UP_ARROW);
+  if(gas2.isHold()) Keyboard.print('w');
+//-----------------------btns----------------------------
+  if(btn1.isHold()) Keyboard.print('p');
+  if(btn2.isHold()) Keyboard.print('q');
+  if(btn3.isHold()) Keyboard.print('');
+  
+//=======================================================
 }
