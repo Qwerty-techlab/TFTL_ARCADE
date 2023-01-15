9bc6a9fab645c09d4ec838f4925b9c6f7c05d6cd
Qwerty-techlab <85824350+Qwerty-techlab@users.noreply.github.com>
2023-01-15 12:02:30 +0700

    Update TFTL_ARCADE.ino

--- TFTL_ARCADE/TFTL_ARCADE.ino
+++ TFTL_ARCADE/TFTL_ARCADE.ino
@@ -1,44 +1,43 @@
 /*
   TFTL-ARCADE
-
   this sketch works only on Arduino Leonardo 
   and similar bords (like as pro micro).
-
   created 24 Dec 2022
-  modified 28 Dec 2022
+  modified 12 Jan 2023
   by Kirill Chefonov
-
   GitHub: https://github.com/Qwerty-techlab
   VK: https://vk.com/alkekseevich 
 */
 
 //--------------settings------------------
-#define BTN_1 1 //left btn
-#define BTN_2 2 //right btn
-#define BTN_3 3 //start btn
-#define GAS_1 4 
-#define GAS_2 5
+#define BTN_1 2 //left btn
+#define BTN_2 3 //right btn
+#define BTN_3 4 //start btn
+#define GAS_1 5 
+#define GAS_2 6
 
 //encoder(wheel) pins. 1-left encoder, 2-right encoder
-#define DT1 6  
+#define DT1 7  
 #define DT2 8
-#define SW1 7
-#define SW2 9
+#define SW1 9
+#define SW2 10
 //----------------------------------------
 #include "Keyboard.h"
-
-#include "EncButton2.h"
-EncButton2<EB_BTN> btn1(INPUT, BTN_1);
-EncButton2<EB_BTN> btn2(INPUT, BTN_2);
-EncButton2<EB_BTN> btn3(INPUT, BTN_3);
-EncButton2<EB_BTN> gas1(INPUT, GAS_1);
-EncButton2<EB_BTN> gas2(INPUT, GAS_1);
-EncButton2<EB_ENC> first(INPUT, DT1, SW1);
-EncButton2<EB_ENC> second(INPUT, DT2, SW2);
+#include "GyverEncoder.h"
+#include "GyverButton.h"
+GButton btn1(BTN_1);
+GButton btn2(BTN_2);
+GButton btn3(BTN_3);
+GButton gas1(GAS_1);
+GButton gas2(GAS_2);
+Encoder first(DT1, SW1);
+Encoder second(DT2, SW2);
 //=========================================
 void setup() {
   Keyboard.begin();
   Serial.begin(9600);
+  first.setType(TYPE2);
+  second.setType(TYPE2);
 }
 
 void loop(){
@@ -55,17 +54,17 @@ void loop(){
 //=======================================================
 
 //-----------------------wheel---------------------------
-  if(first.right()) Keyboard.print(KEY_RIGHT_ARROW);
-  if(first.left()) Keyboard.print(KEY_LEFT_ARROW);
-  if(second.right()) Keyboard.print('a');
-  if(second.left()) Keyboard.print('d');
+  if(first.isRight()) Keyboard.print('z');
+  if(first.isLeft()) Keyboard.print('x');
+  if(second.isRight()) Keyboard.print('a');
+  if(second.isLeft()) Keyboard.print('d');
 //----------------------pedals---------------------------
-  if(gas1.hold()) Keyboard.print(KEY_UP_ARROW);
-  if(gas2.hold()) Keyboard.print('w');
+  if(gas1.isHold()) Keyboard.print('e');
+  if(gas2.isHold()) Keyboard.print('w');
 //-----------------------btns----------------------------
-  if(btn1.click()) Keyboard.print('p');
-  if(btn2.click()) Keyboard.print('q');
-  if(btn3.click()) Keyboard.print(' ');
+  if(btn1.isClick()) Keyboard.print('p');
+  if(btn2.isClick()) Keyboard.print('q');
+  if(btn3.isClick()) Keyboard.print(' ');
   
 //=======================================================
 }
